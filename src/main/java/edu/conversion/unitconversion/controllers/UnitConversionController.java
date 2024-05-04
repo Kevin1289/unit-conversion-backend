b49487e93d029ed353ad8ecaa44d65cca1063d4b
Kevin Wu <sxw207@nyu.edu>
2024-05-04 02:49:49 -0400

    feat: add checkstyle integration

--- src/main/java/edu/conversion/unitconversion/controllers/UnitConversionController.java
+++ src/main/java/edu/conversion/unitconversion/controllers/UnitConversionController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UnitConversionController {
 
+    /**
+     * This method is a simple endpoint for unit conversion.
+     * Currently, it just prints "Hi" and returns "Unit Conversion".
+     *
+     * @return a string message
+     */
     @GetMapping("/unitconversion")
     public String unitConversion() {
+        System.out.println("Hi");
         return "Unit Conversion";
     }
 
