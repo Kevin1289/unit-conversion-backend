26fb45b37493bbfb99e13904d1e978978efcdb93
Kevin Wu <sxw207@nyu.edu>
2024-05-04 19:00:57 -0400

    feat: add conversion controller tests

--- src/main/java/edu/conversion/unitconversion/controller/UnitConversionController.java
+++ src/main/java/edu/conversion/unitconversion/controller/UnitConversionController.java
@@ -3,20 +3,18 @@ package edu.conversion.unitconversion.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.conversion.unitconversion.dto.UnitConversionResponse;
+import edu.conversion.unitconversion.enums.ResponseStatus;
+
 @RestController
 public class UnitConversionController {
 
     /**
      * This method is a simple endpoint for unit conversion.
-     * Currently, it just prints "Hi" and returns "Unit Conversion".
-     *
-     * @return a string message
      */
     @GetMapping("/unitconversion")
-    public String unitConversion() {
-        // print The temperature from Celsius to Fahrenheit is: 33.8
-        System.out.println("The temperature from Celsius to Fahrenheit is: " + 0);
-        return "Unit Conversion";
+    public UnitConversionResponse unitConversion() {
+        return new UnitConversionResponse(ResponseStatus.INVALID);
     }
 
 }
