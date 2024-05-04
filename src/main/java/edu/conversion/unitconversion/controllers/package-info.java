1ef52a570c59957ac92f51700be63f58da7755d8
Kevin Wu <65782567+Kevin1289@users.noreply.github.com>
2024-05-04 02:51:05 -0400

    feat: add checkstyle integration (#2)

---
+++ src/main/java/edu/conversion/unitconversion/controllers/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains the controllers for the unit conversion application.
+ *
+ * @author Kevin Wu
+ * @version 1.0
+ * @since 2024-05-04
+ */
+package edu.conversion.unitconversion.controllers;
