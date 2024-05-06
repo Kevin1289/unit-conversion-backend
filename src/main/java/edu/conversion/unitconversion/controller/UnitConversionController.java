d83fefa1ae5a2dca3948de492823654ecdabb899
Kevin Wu <sxw207@nyu.edu>
2024-05-06 11:59:05 -0400

    feat: add swagger endpoint

--- src/main/java/edu/conversion/unitconversion/controller/UnitConversionController.java
+++ src/main/java/edu/conversion/unitconversion/controller/UnitConversionController.java
@@ -9,26 +9,30 @@ import edu.conversion.unitconversion.dto.UnitConversionResponse;
 import edu.conversion.unitconversion.enums.ResponseStatus;
 import edu.conversion.unitconversion.service.UnitConversionService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 public class UnitConversionController {
 
     /**
      * This method is a simple endpoint for unit conversion.
-     * @param type: type of conversion
-     * @param value: value to convert
-     * @param unit: unit of value
-     * @param target: target unit
+     * 
+     * @param type:     type of conversion
+     * @param value:    value to convert
+     * @param unit:     unit of value
+     * @param target:   target unit
      * @param response: student response value
      * 
      * @return UnitConversionResponse
      */
+    @Operation(summary = "Convert units", description = "This is a simple endpoint for unit conversion.")
     @GetMapping("/unitconversion")
     public UnitConversionResponse unitConversion(@RequestParam(value = "type", required = true) String type,
             @RequestParam(value = "value", required = true) double value,
             @RequestParam(value = "unit", required = true) String unit,
             @RequestParam(value = "target", required = true) String target,
             @RequestParam(value = "response", required = true) double response) {
-        
+
         try {
             UnitConversionRequest request = new UnitConversionRequest(type, value, unit, target, response);
 
@@ -38,4 +42,10 @@ public class UnitConversionController {
         }
     }
 
+    // Test endpoint
+    @Operation(summary = "Test endpoint", description = "This is a test endpoint.")
+    @GetMapping("/test")
+    public String test() {
+        return "Test endpoint";
+    }
 }
