7fb76c74ec1fd3ea904b97d12bdd07cf22636e64
Kevin Wu <sxw207@nyu.edu>
2024-05-07 22:20:42 -0400

    test: add missing test and fix format

---
+++ src/test/java/edu/conversion/unitconversion/UnitConversionApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.conversion.unitconversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UnitConversionApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
